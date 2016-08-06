a9bab0bb945cacd5b8b00e1f8bab9264be147470
Julie Herrick <JohnMac@johns-macbook-pro-2.local>
2016-08-06 10:42:42 -0500

    Initial Commit

---
+++ This_or_ThisTests/This_or_ThisTests.swift
@@ -0,0 +1,36 @@
+//
+//  This_or_ThisTests.swift
+//  This_or_ThisTests
+//
+//  Created by John Herrick on 8/6/16.
+//  Copyright © 2016 John Herrick. All rights reserved.
+//
+
+import XCTest
+@testable import This_or_This
+
+class This_or_ThisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
