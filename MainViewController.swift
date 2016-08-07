fc864e1253b84a58427bb76a9e9369ec2abb9916
Jrh0203 <herjo345@gmail.com>
2016-08-07 03:01:04 -0500

    Casual Update

--- MainViewController.swift
+++ MainViewController.swift
@@ -15,22 +15,33 @@ class Quest {
     var question: String
     var left: Int
     var right: Int
+    var leftWord: String
+    var rightWord: String
+    var leftCaption: String
+    var rightCaption: String
     
-    init(id: Int, question: String, left: Int, right: Int) {
+    init(id: Int, question: String, left: Int, right: Int, leftWord: String, rightWord: String, leftCaption: String, rightCaption: String) {
         self.id = id
         self.question = question
         self.left=left
         self.right=right
+        self.leftWord=leftWord
+        self.rightWord=rightWord
+        self.leftCaption=leftCaption
+        self.rightCaption=rightCaption
     }
 }
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var leftButton: UILabel!
+    @IBOutlet weak var rightButton: UILabel!
+    @IBOutlet weak var leftImage: UIImageView!
+    @IBOutlet weak var rightImage: UIImageView!
     @IBOutlet weak var qlabel: UILabel!
-    @IBOutlet weak var leftButton: UIButton!
-    @IBOutlet weak var rightButton: UIButton!
     var screenNumber: NSString?
     var currQuestion: String?
     var num: Int = 0
+    var someInts = [Quest]()
     
     
     
@@ -39,7 +50,13 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         num=0;
-        var someInts = [Quest]()
+        
+            let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("imageTappedLeft:"))
+            leftImage.userInteractionEnabled = true
+            leftImage.addGestureRecognizer(tapGestureRecognizer)
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target:self, action:Selector("imageTappedRight:"))
+        rightImage.userInteractionEnabled = true
+        rightImage.addGestureRecognizer(tapGestureRecognizer2)
         
         Alamofire.request(.GET, "http://172.28.249.99/tot/getQuestions1.php")
             .responseJSON { response in
@@ -60,13 +77,35 @@ class MainViewController: UIViewController {
                             //Do something you want
                             //print(index)
                             //print(subsubJson)
-                            var q = Quest(id: Int(subsubJson["id"].stringValue)!, question: subsubJson["content"].stringValue, left: subsubJson["yes"].int!, right: subsubJson["no"].int!)
-                            someInts.append(q)
+                            var q = Quest(id: Int(subsubJson["id"].stringValue)!, question: subsubJson["content"].stringValue, left: subsubJson["yes"].int!, right: subsubJson["no"].int!, leftWord: subsubJson["leftImage"].stringValue, rightWord: subsubJson["rightImage"].stringValue, leftCaption: subsubJson["leftText"].stringValue, rightCaption: subsubJson["rightText"].stringValue)
+                            self.someInts.append(q)
                         }
                         
                     }
-                    print(someInts[self.num].question)
-                    self.qlabel.text=someInts[self.num].question
+                    
+                    let url = NSURL(string: "http://172.28.249.99/images/img0.png")
+                    let url2 = NSURL(string: "http://172.28.249.99/images/img1.png")
+                    
+                    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+                        let data = NSData(contentsOfURL: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.leftImage.image = UIImage(data: data!)
+                            print("WOAH DUDE")
+                        });
+                    }
+                    
+                    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+                        let data2 = NSData(contentsOfURL: url2!) //make sure your image in this url does exist, otherwise unwrap in a if let check
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.rightImage.image = UIImage(data: data2!)
+                            print("WOAH DUDE3")
+                        });
+                    }
+
+
+                    
+                    print(self.someInts[self.num].question)
+                    self.qlabel.text=self.someInts[self.num].question
                     
                     //print (info)
                     //for (key,subJson):(String, JSON) in json {
@@ -79,26 +118,45 @@ class MainViewController: UIViewController {
                     //}
                     
                 }
-                print(someInts)
+                print(self.someInts)
+
                 
         }
         
-        /*
-        Alamofire.request(.GET, "http://172.28.249.99/tot/test.php")
-            .validate()
-            .responseString { response in
-                print("Success: \(response.result.isSuccess)")
-                print("Response String: \(response.result.value)")
-                self.currQuestion = response.result.value
-                self.qlabel.text=self.currQuestion
-        }
-*/
-        //currQuestion = Alamofire.request(.GET, "http://172.28.249.99/tot/getQuestions.php")
-        //self.qlabel.text = currQuestion
+    }
+    
+    func imageTappedLeft(img: AnyObject)
+    {
+        // Your action
+        print("tapped left")
+        Alamofire.request(.GET, "http://172.28.249.99/tot/vote.php", parameters:["questionid":someInts[self.num].id, "vote":"YES"])
         
-        // Do any additional setup after loading the view.
+    }
+    func imageTappedRight(img: AnyObject)
+    {
+        // Your action
+        print("tapped right")
+        Alamofire.request(.GET, "http://172.28.249.99/tot/vote.php", parameters:["questionid":someInts[self.num].id, "vote":"NO"])
+    }
+    
+    func updateThings(){
+        print("update")
+        self.leftButton.text=(someInts[self.num].leftCaption)
+        print(self.someInts[self.num].leftCaption)
+        self.rightButton.text=(someInts[self.num].rightCaption)
+        self.qlabel.text=someInts[self.num].question
     }
 
+    @IBAction func swipeRight(sender: UISwipeGestureRecognizer) {
+        if (self.num>0){
+            self.num--}
+        updateThings()
+    }
+    @IBAction func swipeLeft(sender: UISwipeGestureRecognizer) {
+        if (self.num<self.someInts.count-1){
+            self.num++}
+        updateThings()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- This_or_This/AddNewViewController.swift
+++ This_or_This/AddNewViewController.swift
@@ -9,23 +9,74 @@
 import UIKit
 import Alamofire
 
-class AddNewViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    @IBOutlet weak var rightAddImage: UIImageView!
+    @IBOutlet weak var leftAddImage: UIImageView!
+    @IBOutlet weak var leftAddOption: UITextField!
+    @IBOutlet weak var rightAddOption: UITextField!
     @IBOutlet weak var questionField: UITextField!
     @IBOutlet weak var doneButton: UIBarButtonItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     var name: NSString?
     var question: NSString?
+    var leftCaption: NSString?
+    var rightCaption: NSString?
+    var imagePicker: UIImagePickerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Username: \(name!)")
+        let imagePicker: UIImagePickerController! = UIImagePickerController()
+        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("imageTappedLeft:"))
+        leftAddImage.userInteractionEnabled = true
+        leftAddImage.addGestureRecognizer(tapGestureRecognizer)
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target:self, action:Selector("imageTappedRight:"))
+        rightAddImage.userInteractionEnabled = true
+        rightAddImage.addGestureRecognizer(tapGestureRecognizer2)
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imageTappedLeft(img: AnyObject)
+    {
+        // Your action
+        print("tapped left")
+        if (UIImagePickerController.isSourceTypeAvailable(.Camera)) {
+            if UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil {
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .Camera
+                imagePicker.cameraCaptureMode = .Photo
+                presentViewController(imagePicker, animated: true, completion: {})
+            } else {
+                //print("Rear camera doesn't exist", message: "Application cannot access the camera.")
+            }
+        } else {
+            //print("Camera inaccessable", message: "Application cannot access the camera.")
+        }
+        
+    }
+    func imageTappedRight(img: AnyObject)
+    {
+        // Your action
+        print("tapped right")
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("Got an image")
+        if let pickedImage:UIImage = (info[UIImagePickerControllerOriginalImage]) as? UIImage {
+            let selectorToCall = Selector("imageWasSavedSuccessfully:didFinishSavingWithError:context:")
+            UIImageWriteToSavedPhotosAlbum(pickedImage, self, selectorToCall, nil)
+        }
+        imagePicker.dismissViewControllerAnimated(true, completion: {
+            // Anything you want to happen when the user saves an image
+        })
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        print("User canceled image")
+        dismissViewControllerAnimated(true, completion: {
+            // Anything you want to happen when the user selects cancel
+        })
     }
     
 
@@ -40,8 +91,10 @@ class AddNewViewController: UIViewController {
     */
     @IBAction func donePressed(sender: AnyObject) {
         question=self.questionField.text
+        leftCaption=self.leftAddOption.text
+        rightCaption=self.rightAddOption.text
         print("Question: \(question!)")
-        print(Alamofire.request(.GET, "http://172.28.249.99/tot/newQuestion.php", parameters:["name":name!, "question":question!]))
+        print(Alamofire.request(.GET, "http://172.28.249.99/tot/newQuestion.php", parameters:["name":name!, "question":question!, "leftText":leftCaption!, "rightText":rightCaption!]))
         
         dismissViewControllerAnimated(true, completion: nil)
         
